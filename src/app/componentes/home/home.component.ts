8010ce60ee7ce9a7401db1a99befad7947868d89
Fabianuseche <115897847+Fabianuseche@users.noreply.github.com>
2023-09-29 17:11:37 -0500

    sort

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -33,6 +33,10 @@ export class HomeComponent {
   { title: 'REINICIAR EL JUEGO',  showFront: true, }
  ]
 
+ ngOnInit(){
+  this.Sort()
+ }
+
  Sort(){
   for (let i = this.cards.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
